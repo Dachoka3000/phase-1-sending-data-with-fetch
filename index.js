2842d514bf053148dc98b556e40352642eaffaeb
Machoka Daisy <machokadaisy25@gmail.com>
2022-12-07 13:59:31 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+let newUser = {
+    name: "Daisy",
+    email: "daisy@email.com"
+}
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method:'POST',
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const work = data.id
+        document.body.append(work)
+    })
+    .catch(error=>{
+        const newError = error.message
+        document.body.append(newError)
+    })
+}
